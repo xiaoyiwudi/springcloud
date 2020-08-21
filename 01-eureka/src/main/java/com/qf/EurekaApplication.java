d4eb74b4521aebf7eedc3a80b07fe457af1a2643
pinkpig <1844618562@qq.com>
2020-08-21 19:59:39 +0800

    gg

---
+++ 01-eureka/src/main/java/com/qf/EurekaApplication.java
@@ -0,0 +1,20 @@
+/**
+ * Author:ysx
+ * Date:2020/8/14/01417:00
+ * FileName:EurekaApplication
+ */
+
+
+package com.qf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
